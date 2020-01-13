b624a7d976724242fbe99fc9f511a16d9995c42a
ChMcg <Bakhir.A.D.st@gmail.com>
2020-01-13 21:59:06 +0300

    some

--- HW7.py
+++ HW7.py
@@ -16,6 +16,11 @@ def main():
     passwd = open('/etc/passwd', 'r')
     groups = open('/etc/group', 'r')
     lines = passwd.readlines()
+    # id_s = dict.fromkeys([x.strip().split(':')[2] for x in lines], [])
+    id_s = {}
+    for i in lines:
+        temp_p = i.strip().split(':')
+        id_s[temp_p[0]] = temp_p[2]
     temp = [x.strip().split(':')[6] for x in lines]
     interps = dict.fromkeys(temp, 0)
     for item in temp: 
@@ -23,6 +28,21 @@ def main():
     print('( {} )'\
         .format(" ; "\
         .join([str(x) + ' - ' + str(interps[x]) for x in interps.keys()])))
+    lines = groups.readlines()
+    # group_names = [x.strip().split(':')[0] for x in lines]
+    # group_users = dict.fromkeys(group_names, [])
+    group_users = {}
+    for item in lines:
+        temp = item.strip().split(':')
+        # group_names.append()
+        if temp[3] != '':
+            group_users[temp[0]] = temp[3].split(',')
+    print('( {} )'\
+        .format(', '\
+        .join(['{}:{}'\
+        .format(x, ','.join([id_s[name] for name in group_users[x]])) for x in group_users.keys()])))
+
+    
     
     
 
