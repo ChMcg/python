46f3075dd4b484711d9d2f8517b7ae6058332757
Bakhir A.D <bakhir.a.d.st@gmail.com>
2020-01-17 18:45:41 +0300

    some

--- hw9.py
+++ hw9.py
@@ -22,33 +22,30 @@ def problem9() -> int:
 
 def problem6() -> int:
     """
-    The sum of the squares of the first ten natural numbers is,
+    Problem 6
+        The sum of the squares of the first ten natural numbers is,
 
-    1^2+2^2+...+10^2=385
-    The square of the sum of the first ten natural numbers is,
+        1^2+2^2+...+10^2=385
+        The square of the sum of the first ten natural numbers is,
 
-    (1+2+...+10)^2=55^2=3025
-    Hence the difference between the sum of the squares of the first ten natural numbers and
-    the square of the sum is 3025−385=2640.
+        (1+2+...+10)^2=55^2=3025
+        Hence the difference between the sum of the squares of the first ten natural numbers and
+        the square of the sum is 3025−385=2640.
 
-    Find the difference between the sum of the squares of the first one hundred natural
-    numbers and the square of the sum.
+        Find the difference between the sum of the squares of the first one hundred natural
+        numbers and the square of the sum.
     """
     return sum([x for x in range(100+1)])**2 - sum([x**2 for x in range(100+1)])
 
 
-def meas(s: str, func) -> None:
+def meas(name: str, func) -> None:
     start = datetime.now()
-    print(f'{s}: {func()}')
+    print(f'{name}: {func()}')
     stop = datetime.now()
     print(f'Время: {stop - start}')
 
 
 def main():
-    # start = datetime.now()
-    # print(f'p9: {problem9()}')
-    # stop = datetime.now()
-    # print(f'Время: {stop - start}')
     meas('Problem 9', problem9)
     meas('Problem 6', problem6)
 
