e71f873c04f21f04207c4ad14bc6aff5fa2ed038
Bakhir A.D <bakhir.a.d.st@gmail.com>
2020-01-19 00:23:40 +0300

    hw9 some

--- hw9.py
+++ hw9.py
@@ -67,7 +67,9 @@ def problem40() -> int:
     """
     # some = reduce(lambda x, y: x + str(y), [a for a in range(1_000_000)], "")
     # some = reduce(lambda x, y: x + [int(c) for c in y], [str(a) for a in range(1_000_000)], [])
-    some = reduce(lambda x, y: x + y, [[int(k) for k in str(a)] for a in range(1_000_000)], [])
+    # some = reduce(lambda x, y: x + y, [[int(k) for k in str(a)] for a in range(1_000_000)], [])
+    some = [[int(k) for k in str(a)] for a in range(1_000_000)]
+    some = reduce(lambda x, y: x + y, some, [])
     return int(some[1]) * int(some[10]) * int(some[100]) * int(some[1000]) * int(some[10000]) * int(some[100000]) * int(some[1000000])
 
 def meas(name: str, func) -> None:
